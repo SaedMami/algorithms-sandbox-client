4c4f971f3416392796b3a068ad1e606cc1081782
Saed Mami <saed.mami@gmail.com>
2020-09-06 09:57:36 +0100

    change the color

--- src/visualisation/Renderers/GraphRenderer.tsx
+++ src/visualisation/Renderers/GraphRenderer.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { DataSet, Network } from "vis-network/standalone";
-import { GraphFrame } from "../../tracers/graph/GraphFrames";
+import { GraphFrame, NodeMarkerFrame } from "../../tracers/graph/GraphFrames";
 
 type props = {
   frame: GraphFrame;
@@ -13,7 +13,11 @@ const GraphRenderer = ({ frame }: props) => {
   var edges = new Array<Object>();
 
   Array.from(frame.graphList).forEach(([nodeId, nodeEdges]) => {
-    nodes.push({ id: nodeId, label: String(nodeId) });
+    nodes.push({
+      id: nodeId,
+      label: String(nodeId),
+      color: getNodeColor(frame.markers, nodeId),
+    });
     nodeEdges.forEach((edge) => {
       edges.push({ from: nodeId, to: edge });
     });
@@ -30,6 +34,11 @@ const GraphRenderer = ({ frame }: props) => {
     var options = {
       height: "100%",
       width: "100%",
+      edges: {
+        color: {
+          inherit: false,
+        },
+      },
       layout: {
         randomSeed: 123,
         improvedLayout: true,
@@ -46,6 +55,21 @@ const GraphRenderer = ({ frame }: props) => {
   });
 
   return <div style={{ width: "100%", height: "70vh" }} ref={graphEl}></div>;
+};
+
+const getNodeColor = (markers: Map<string, NodeMarkerFrame>, index: number) => {
+  // return the first applicable color for the node
+  const it = markers.values();
+  let marker = it.next();
+  while (!marker.done) {
+    if (marker.value.selected[index] === 1) {
+      return marker.value.color;
+    }
+    marker = it.next();
+  }
+
+  // no marker found for this element
+  return null;
 };
 
 export default GraphRenderer;
